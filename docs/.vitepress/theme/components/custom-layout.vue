7da213ef1efe5ceb73983b47a69c148712274baf
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-05-20 14:48:36 +0800

    basic support for search bar

---
+++ docs/.vitepress/theme/components/custom-layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import SearchBar from './search-bar.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #nav-bar-content-before>
+      <SearchBar />
+    </template>
+  </Layout>
+</template>
